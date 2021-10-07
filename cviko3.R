c92156f89eedafa4b118e7307053d348dc1e0959
Vendy <xbehun04@vutbr.cz>
2021-10-07 13:19:17 +0200

    Upravena funkce VratitMince.

--- cviko3.R
+++ cviko3.R
@@ -1,10 +1,11 @@
-VratitMince <- function(M){
-  # M je to, co mam vratit
-  # z50 <- M %% 50 #zbytek po deleni 50
-  # p50 <- (M - z50)/50 #pocet 50 minci
-  mince <- c(50,20,10,5,2,1)
-  pocet_minci <- matrix(0,1,6)
+VratitMince <- function(M, mince){
+  # M je to, co mam vratit, mince jsou vektorem minci, ktere jsou pouzite.
+  # Pr. VratitMince(423, c(50,20,10,5,2,1))
+  
+  pocet_minci <- matrix(0,1,length(mince))
   zbytek <- M
+  
+  # Vracim od nejvetsich (napr od 50).
   for (i in 1:length(mince)) {
   pocet <- floor(zbytek/mince[i]) #pocet 50 korunovych minci
   zbytek <- zbytek %% mince[i] # zbyva vratit
@@ -13,4 +14,3 @@ VratitMince <- function(M){
   return(pocet_minci)
 }
 
-# mince 50, 20, 10, 5, 2, 1
