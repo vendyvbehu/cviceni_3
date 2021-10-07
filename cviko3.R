24029fa77a9ed2b1330973db7e155029d35ba70b
Vendy <xbehun04@vutbr.cz>
2021-10-07 14:06:06 +0200

    Skript obsahujici funkce VratitMince a Cokolada v rekurzivni podobe.

--- cviko3.R
+++ cviko3.R
@@ -1,3 +1,5 @@
+# Problem s vracenim minci
+
 VratitMince <- function(M, mince){
   # M je to, co mam vratit, mince jsou vektorem minci, ktere jsou pouzite.
   # Pr. VratitMince(423, c(50,20,10,5,2,1))
@@ -14,3 +16,22 @@ VratitMince <- function(M, mince){
   return(pocet_minci)
 }
 
+# Nejcokoladovejsi cesta
+
+Cokolada <- function(M,r,s){
+  # M reprezentuje labyrint, r je radek a s sloupec
+  # Priklad matrix(c(3,0,0,0,1,4,0,0,5,3,0,0,1,2,6,7), nrow = 4, ncol = 4, byrow = TRUE)
+  
+  # Rekurzivni zpusob
+  if (r == nrow(M)){
+    return(M[r,s])
+  }
+  else{
+    C <- M[r,s]
+    Cdolu <- Cokolada(M,r+1,s)
+    Csikmo <- Cokolada(M,r+1,s+1)
+    return(max(Cdolu,Csikmo)+C)
+  }
+}
+
+# Hanojske veze
